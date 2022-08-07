d048052a4b0c5e0442c53707506dc38b1017b6b0
AC Putranto <aldyansyahcp@gmail.com>
2022-08-08 04:37:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ def hom():
 @app.route("/tests",methods=["GET","POST"])
 def cob():
     data = [{'id': ('1', '2', '3'), 'name': ('bhanu', 'sivanagulu'), 'department': ('HR', 'IT')}, {'id': ('4', '5', '6'), 'name': ('sai', 'poori'), 'department': ('HR', 'IT')}, {'id': ('7', '8', '9'), 'name': ('teja', 'gowtam'), 'department': ('finance', 'IT')}, {'id': ('10', '11', '12'), 'name': ('sai', 'jyothi'), 'department': ('business', 'IT')}, {'id': ('13', '14', '15'), 'name': ('prudhvi', 'nagendram'), 'department': ('business', 'IT')}]
-    return data
+    f = json.dumps(data, indent=2)
+    return f
+
 @app.route("/ytvideo-sound", methods=["GET", "POST"])
 def yts():
         try:
