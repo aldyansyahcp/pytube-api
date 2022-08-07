377a10d88216fee4f2188e549fe9fccd9f5542b0
AC Putranto <aldyansyahcp@gmail.com>
2022-08-08 04:33:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from pytube import YouTube as yt
-import random
+import random, json
 from flask import Flask, render_template, request,jsonify
 
 app= Flask(__name__)
@@ -8,6 +8,10 @@ app= Flask(__name__)
 def hom():
     return render_template("index.html")
 
+@app.route("/tests",methods=["GET","POST"])
+def cob():
+    data = [{'id': ('1', '2', '3'), 'name': ('bhanu', 'sivanagulu'), 'department': ('HR', 'IT')}, {'id': ('4', '5', '6'), 'name': ('sai', 'poori'), 'department': ('HR', 'IT')}, {'id': ('7', '8', '9'), 'name': ('teja', 'gowtam'), 'department': ('finance', 'IT')}, {'id': ('10', '11', '12'), 'name': ('sai', 'jyothi'), 'department': ('business', 'IT')}, {'id': ('13', '14', '15'), 'name': ('prudhvi', 'nagendram'), 'department': ('business', 'IT')}]
+    return data
 @app.route("/ytvideo-sound", methods=["GET", "POST"])
 def yts():
         try:
